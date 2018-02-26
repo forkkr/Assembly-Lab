ec2cd23929832118dc0a38799cd89085713ebec4
Kashob Roy <kashobroy@gmail.com>
2018-02-26 14:54:11 +0600

    Add files via upload

---
+++ while.asm
@@ -0,0 +1,54 @@
+segment .data
+fmt: dq "%lld ",10,0
+ssfmt: dq "%lld",0
+
+
+segment .bss
+a: resq 255
+b: resq 2
+c: resq 2
+d: resq 2
+cn: resq 2
+
+segment .text
+global main
+extern printf
+extern scanf
+
+main:
+push RBP
+xor rcx, rcx
+mov [c] , rcx
+xor rax , rax
+mov rdi, ssfmt
+mov rsi , cn
+call scanf
+ll:
+mov rbx , rcx
+mov rcx, [b]
+mov r8 , [cn]
+cmp rcx ,r8
+je pr
+xor rax, rax
+mov rdi , ssfmt
+mov rsi, d
+call scanf
+mov rax , [d]
+mov [a+rbx*8],rax
+xor rax , rax
+add rax, [c]
+add rax, [d]
+mov [c], rax
+mov rcx , [b]
+INC rcx
+mov [b], rcx
+jmp ll
+
+pr:
+xor rax , rax
+mov rdi , fmt
+mov rsi , [c]
+call printf
+pop RBP
+ret
+
